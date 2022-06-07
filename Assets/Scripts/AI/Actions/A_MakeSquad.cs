3f97b80b6c5c391f16be09457d7e93b286afc59d
qbleuse <q.bleuse@student.isartdigital.com>
2022-06-07 12:28:36 +0200

    remove log, new version of squads create

--- Assets/Scripts/AI/Actions/A_MakeSquad.cs
+++ Assets/Scripts/AI/Actions/A_MakeSquad.cs
@@ -19,20 +19,6 @@ public class A_Squad_Data : AIActionData
 
 }
 
-/*class Possible_Target
-{
-    public TargetBuilding target = null;
-    public float dist = 0.0f;
-    public int TargetFind = 0; 
-
-    public void Set(TargetBuilding _target, float _dist)
-    {
-        target = _target;
-        dist = _dist;
-        TargetFind = 1;
-    }
-}*/
-
 [CreateAssetMenu(fileName = "MakeSquad", menuName = "Actions/Squad")]
 public class A_MakeSquad : AIAction
 {
@@ -53,24 +39,8 @@ public class A_MakeSquad : AIAction
     {
         if (data is A_Squad_Data package)
         {
-			//List<Possible_Target> PossibleAttack = new List<Possible_Target>();
-			//foreach (var unit in package.army.UnitList)
-			//{
-			//    Possible_Target targetActu = new Possible_Target();
-			//    foreach (var building in package.targets)
-			//    {
-			//        float dist = (unit.transform.position - building.transform.position).magnitude;
-			//        if (dist <= Radius)
-			//        {
-			//            if (targetActu.TargetFind == 0)
-			//                targetActu.Set(building, dist);
-			//            else if (targetActu.dist > dist)
-			//                targetActu.Set(building, dist);
-			//            
-			//            PossibleAttack.Add(targetActu);
-			//        }
-			//    }
-			//}
+			if (package.army.UnitList.Count == 0)
+				return false;
 
 			List<Unit> availableUnit		= new List<Unit>(package.army.UnitList);
 			List<Vector3> avgDesiredUnitPos = new List<Vector3>();
@@ -82,11 +52,15 @@ public class A_MakeSquad : AIAction
 				Vector3 unitAvgPos = Vector3.zero;
 
 				/* add pos of closest point of interest */
-				unitAvgPos += package.EnemyArmy.SquadList.OrderBy(e => (e.Position - iUnit.transform.position).sqrMagnitude).First().Position;
-				unitAvgPos += package.EnemyArmy.FactoryList.OrderBy(e => (e.transform.position - iUnit.transform.position).sqrMagnitude).First().transform.position;
-				unitAvgPos += package.targets.OrderBy(e => (e.transform.position - iUnit.transform.position).sqrMagnitude).First().transform.position;
+				if (package.EnemyArmy.SquadList.Count > 0)
+					unitAvgPos += package.EnemyArmy.SquadList.OrderBy(e => (e.Position - iUnit.transform.position).sqrMagnitude).First().Position;
+				if (package.EnemyArmy.FactoryList.Count > 0)
+					unitAvgPos += package.EnemyArmy.FactoryList.OrderBy(e => (e.transform.position - iUnit.transform.position).sqrMagnitude).First().transform.position;
+				if (package.targets.Length > 0)
+					unitAvgPos += package.targets.OrderBy(e => (e.transform.position - iUnit.transform.position).sqrMagnitude).First().transform.position;
 
-				/* cmpute the avg desired pos */
+				/* compute the avg desired pos (we don't care if it is not the real pos,
+				 * we're just searching the closest point to each unit for those available) */
 				unitAvgPos /= 3;
 
 				avgDesiredUnitPos.Add(unitAvgPos);
@@ -151,8 +125,9 @@ public class A_MakeSquad : AIAction
 			}
 
             Debug.Log("army:" + package.army.SquadList.Count);
+			Debug.Log("army:" + package.army.SquadList[0].Count);
 
-            return true;
+			return true;
         }
 
         else
@@ -166,33 +141,36 @@ public class A_MakeSquad : AIAction
     {
         if (data is A_Squad_Data package)
         {
-            int TotalUnit = 0;
-            foreach (var factory in package.army.FactoryList)
-            {
-                if (factory.IsBuildingUnit)
-                {
-                    TotalUnit += factory.UnitInQueue;
-                }
-            }
+			if (package.army.UnitList.Count > 0)
+			{
+				int MedCost = package.army.TotalCost() / package.army.UnitList.Count;
 
-            TotalUnit += package.army.UnitList.Count;
-            int MedCost = 0;
-            if (TotalUnit > 0)
-            {
-                MedCost = package.army.TotalCost() / TotalUnit;
-                foreach (var squad in package.army.SquadList)
-                {
-                    if (squad.GetCost() < MedCost)
-                    {
-                        _priority = Seuil;
-                        return;
-                    }
-                }
+				if (package.army.SquadList.Count <= 0)
+				{
+					_priority = Seuil;
+					Debug.Log(_priority);
+					return;
+				}
 
-                _priority = 0;
-            }
-            else
-                _priority = 0;
+				if (package.army.CostPending() > 0)
+				{
+					_priority = Seuil;
+					return;
+				}
+
+				foreach (var squad in package.army.SquadList)
+				{
+					if (squad.GetCost() < MedCost)
+					{
+						_priority = Seuil;
+						return;
+					}
+				}
+
+				_priority = 0;
+			}
+			else
+				_priority = 0;
         }
 
     }
